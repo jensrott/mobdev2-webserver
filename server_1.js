9fd3c3efb2bedfd2090a19f20ab4cb547b896f4f
jensrott <jensrott@student.arteveldehs.be>
2018-02-20 15:41:21 +0100

    cleaning up and routes

--- server_1.js
+++ server_1.js
@@ -1,4 +1,4 @@
-// Import the protocols
+/*** Importing The Protocols ***/
 const http = require('http');
 const express = require('express');
 const path = require('path');
@@ -9,38 +9,99 @@ const app = express();
 const server = http.createServer(app);
 // const url = require('url');
 
-/* Settings */
+/*** Settings ***/
 const nodeEnv = (process.env.NODE_ENV) ? process.env.NODE_ENV :
 'development';
 if(nodeEnv !== 'production') {
   console.log('Do some development stuff!');
 }
-
 const hostName = '127.0.0.1';
 // const hostName = '10.5.140.46';
 const port = '8080';
 
-/* Adding Middleware !!Important!! */
+/*** Adding Middleware !!Important!! ***/
 app.use(express.static(path.join(__dirname, 'client/build'))); // To link to the client map, now we can see our cat ! :)
-app.use(logger('dev'));
+app.use(logger('dev')); // Show the routes in the cmd-line
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); // We can now work with big objects
 
+/*** Routes ***/
 app.get('/', (req, res) => {
   res.send('Hello Express Yes! :)');
 });
 
-/* Middlewares errors handling */
+/* Students routes */
+app.get('/students', (req, res) => {
+  res.json([
+    {
+      "name": "Olivier",
+      "lastName": "De Pauw",
+      "eq": 140,
+      "iq": 118
+    },
+    {
+      "name": "Philippe",
+      "lastName": "Parent",
+      "eq": 66,
+      "iq": 168
+    }
+  ]);
+});
+
+app.get('/students/:studentId', (req, res) => {
+  res.json([
+    {
+      "name": "Olivier",
+      "lastName": "De Pauw",
+      "eq": 140,
+      "iq": 118
+    }
+  ]);
+});
+
+/* Posts routes */
+app.get('/posts', (req, res) => {
+  res.json([
+    {
+      "name": "Post 1",
+      "message": "Test message 1",
+      "id": 1,
+      "timestamp": new Date()
+    },
+    {
+      "name": "Post 2",
+      "message": "Test message 2",
+      "id": 2,
+      "timestamp": new Date()
+    }
+  ]);
+});
+
+app.get('/posts/:postId', (req, res) => {
+  res.json([
+    {
+      "name": "Post 1",
+      "message": "Test message 1",
+      "id": 1,
+      "timestamp": new Date()
+    }
+  ]);
+});
+
+/*** Middlewares errors handling ***/
 app.use((req, res, next) => {
   const err = new Error('Page Not found!');
   err.status = 400;
   next(err);
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.send('error');
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 /* Description of the server
@@ -59,8 +120,7 @@ const server = http.createServer((req, resp) => {
 });
 */
 
-/* Running the server */
+/*** Running the server ***/
 server.listen(port, hostName, () => {
-  console.log(`Node server running at http://
-  ${hostName}:${port}/ !`);
+  console.log(`Node server running at http://${hostName}:${port}/ !`);
 });
