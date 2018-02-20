7fa28f0aaca0059795a617b478aa4802ddde72ea
jensrott <jensrott@student.arteveldehs.be>
2018-02-20 11:54:51 +0100

    url library used and useragent

--- server_1.js
+++ server_1.js
@@ -1,5 +1,6 @@
-// Import the protocol
+// Import the protocols
 const http = require('http');
+const url = require('url');
 
 // Settings
 const hostName = '127.0.0.1';
@@ -7,12 +8,17 @@ const port = '8080';
 
 // Description of the server
 const server = http.createServer((req, resp) => {
+  const userAgent = req.headers['user-agent']; // User agent
+  const urlParts = url.parse(req.url, true); // Url
+  const paramName = (!urlParts.query.name) ? 'New Media Development' : 
+  urlParts.query.name;
+    
   resp.statusCode = 200;
-  resp.setHeader('Content-Type', 'application/json'); 
-  resp.end(JSON.stringify({
-    "message": "Hello JSON!",
-  }));
+  resp.setHeader('Content-Type', 'text/html');
+  resp.end(`<h1>Hello ${paramName}</h1>
+  <p>Your user agent is ${userAgent}</p>`);
 });
+
 
 // Running the server
 server.listen(port, hostName, () => {
