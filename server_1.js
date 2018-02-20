7658bbce7314d5ce332f3b9fc80f0aa405d72eba
jensrott <jensrott@student.arteveldehs.be>
2018-02-20 11:34:14 +0100

    test push

--- server_1.js
+++ server_1.js
@@ -1,3 +1,18 @@
 // Import the protocol
 const http = require('http');
- 
+
+// Settings
+const hostName = '127.0.0.1';
+const port = '8080';
+
+// Description of the server
+const server = http.createServer((req, resp) => {
+  resp.statusCode = 200;
+  resp.setHeader('Content-Type', 'text/plain');
+  resp.end('<h1>Hello New Media Developers!</h1>');
+});
+
+server.listen(port, hostName, () => {
+  console.log(`Node server running at http://
+  ${hostName}:${port}/ !`);
+});
